fe1bb39e4492351b9f987f7b8e24988bc11c41c5
sonihadiwijaya <32594058+sonihadiwijaya@users.noreply.github.com>
2017-11-28 22:45:29 +0700

    Create nama.cpp

---
+++ nama.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int x;
+	string nama1 [x];
+	string nama2 [10];
+	
+	nama1 [0]="Soni hadi";
+	nama2 [0]="Soni hadi";
+	
+	cout<<nama1 [0]<<endl;
+	cout<<nama2 [0];
+	
+	return 0;
+}
